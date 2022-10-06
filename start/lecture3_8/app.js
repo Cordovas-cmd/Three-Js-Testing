1c93a3174600debca86197fc463a51711b250770
samuel cordova <cordovas1337@gmail.com>
2022-10-06 02:36:04 -0500

    added the loading bar and was able to render the plane

--- start/lecture3_8/app.js
+++ start/lecture3_8/app.js
@@ -29,13 +29,14 @@ class App{
 		this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true } );
 		this.renderer.setPixelRatio( window.devicePixelRatio );
 		this.renderer.setSize( window.innerWidth, window.innerHeight );
+        // when loading a gltf object the loader uses mesh standard material.. bc of this set the output encoding propert to THREE.sRGBencoded.
         this.renderer.outputEncoding = THREE.sRGBEncoding;
         container.appendChild( this.renderer.domElement );
 		this.setEnvironment();
 		
         //Add loading bar
 
-        this.LoadingBar = new LoadingBar();
+        this.loadingBar = new LoadingBar();
         this.loadGLTF();
         this.controls = new OrbitControls( this.camera, this.renderer.domElement );
         
@@ -64,27 +65,31 @@ class App{
         // create an instance and a path to find the assets
         
         // The KEY method to a loader is the load method. this take in 4 parameters. 1: url of file, 2: calback for whe it's loaded, 3: progress calback, 4: error callback.
-        const loader = new GLTFLoader().setPath('../../assets/plane');
+        const loader = new GLTFLoader().setPath('../../assets/plane/');
         // parameter 1: url of File 
         loader.load('microplane.glb', 
         //  2: calback for whe it's loaded
-        gltf=> {
+        gltf => {
             // add scene
             this.scene.add(gltf.scene);
             // hide load bar
-            this.LoadingBar.visible = false;
+            this.loadingBar.visible = false;
             //start rendering loop 
             this.renderer.setAnimationLoop(this.render.bind(this) );
             this.plane = gltf.scene;
         },
         // Param 3 Progress callback
         xhr => {
-
+            // xml https request object, divde loaded by total used to upadte loading bar progress.
+            this.loadingBar.progress = (xhr.loaded/xhr.total);
         },
         // Param 4: error callback
         err => {
 
-        })
+            console.error( err);
+
+        }
+        )
         
     }
     
